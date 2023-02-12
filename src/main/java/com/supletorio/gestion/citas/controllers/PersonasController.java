c6b5101455bff7ff9a5b39b8c9ef22276c5f51cf
Francy Ramirez <franrar95@gmail.com>
2023-02-12 02:27:24 -0500

    Se añade Controller de Personas

---
+++ src/main/java/com/supletorio/gestion/citas/controllers/PersonasController.java
@@ -0,0 +1,33 @@
+package com.supletorio.gestion.citas.controllers;
+
+import com.supletorio.gestion.citas.dto.PacienteDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/personas")
+public class PersonasController {
+
+    @PostMapping("/crear-paciente")
+    public ResponseEntity<?> crearPaciente(@RequestBody PacienteDto paciente) {
+        return null;
+    }
+
+    @GetMapping("/listar-pacientes")
+    public ResponseEntity<?> obtenerPacientes() {
+        return null;
+    }
+
+    @PutMapping("/actualizar-paciente/{id}")
+    public ResponseEntity<?> actualizarPaciente(@RequestBody PacienteDto paciente, @PathVariable Long id) {
+        return null;
+    }
+
+    @DeleteMapping("/eliminar-paciente/{id}")
+    public ResponseEntity<?> eliminarPaciente(@PathVariable Long id) {
+        return null;
+    }
+
+
+}
+
